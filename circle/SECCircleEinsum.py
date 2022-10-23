ac7585497c445167f5b95ff8ab8f1d9d665ea33d
Yanbing Gu <yb.gu0990@gmail.com>
2022-10-22 20:15:09 -0400

    Update SECCircleEinsum.py

--- circle/SECCircleEinsum.py
+++ circle/SECCircleEinsum.py
@@ -35,7 +35,7 @@ def quad_l2_integral(f, a, b):
     return (1/(2*np.pi))*quad(f, a, b, limit = 100)[0]
     
 # (L2) Monte Carlo integration
-def monte_carlo_integral(f, a = 0, b = 2*np.pi, N = 1000):
+def monte_carlo_l2_integral(f, a = 0, b = 2*np.pi, N = 1000):
 
     u = np.zeros(N)
     for i in range(len(u)):
@@ -92,6 +92,7 @@ dphis = [dphi_basis(i) for i in range(2*I+1)]
 
 
 # Apply analysis operator T to obtain v_hat_prime
+# Using quad integration
 v_hat_prime = np.empty([2*J+1, 2*K+1], dtype = float)
 
 for i in range(0, 2*J+1):
@@ -103,6 +104,7 @@ v_hat_prime = np.reshape(v_hat_prime, ((2*J+1)*(2*K+1), 1))
 
 
 # Compute c_ijk coefficients
+# Using Quad integration
 c = np.empty([2*I+1, 2*I+1, 2*I+1], dtype = float)
 for i in range(0, 2*I+1):
     for j in range(0, 2*I+1):
@@ -115,6 +117,7 @@ for i in range(0, 2*I+1):
 # %%
 
 # Compute g_kij Riemannian metric coefficients
+# Using quad integration
 g = np.empty([2*I+1, 2*I+1, 2*I+1], dtype = float)
 for i in range(0, 2*I+1):
             for j in range(0, 2*I+1):
@@ -139,6 +142,7 @@ for j in range(0, 2*I+1):
 
 
 # Compute G_ijkl entries for the Gram operator and its dual
+# Using quad integration
 G = np.zeros([2*I+1, 2*I+1, 2*I+1, 2*I+1], dtype = float)
 G = np.einsum('ikm, jlm->ijkl', c, g, dtype = float)
 
@@ -155,6 +159,7 @@ G_dual = np.linalg.pinv(G)
 # %%
 
 # Apply dual Gram operator G^+ to obtain v_hat 
+# Using quad integration
 v_hat = np.matmul(G_dual, v_hat_prime)
 v_hat = np.reshape(v_hat, (2*J+1, 2*K+1))
 
@@ -178,6 +183,7 @@ X_1, Y_1, U_1, V_1 = zip(*TRAIN_V)
 
 
 # Apply oushforward map to v_hat to obtain approximated vector fields
+# Using quad integration
 F_k = np.zeros([2, 2*I+1], dtype = float)
 F_k[1, 1] = 1/np.sqrt(2)
 F_k[0, 2] = 1/np.sqrt(2)
@@ -340,5 +346,114 @@ ax2.plot(sol_true.t, sol_true.y.T[:, 1], color = 'red')
 ax2.plot(sol_sec.t, sol_sec.y.T[:, 1], color = 'blue')
 ax2.set_title('x-coordinates prediction w.r.t. time t (true = red, SEC = blue)')
 
+plt.show()
+# %%
+
+
+# %%
+## MONTE CARLO SECTION
+
+# Apply analysis operator T to obtain v_hat_prime
+# Using Monte Carlo integration
+v_hat_prime_mc = np.empty([2*J+1, 2*K+1], dtype = float)
+
+for i in range(0, 2*J+1):
+    for j in range(0, 2*K+1):
+        f = double_prod(phis[i], dphis[j])
+        v_hat_prime_mc[i, j] = monte_carlo_l2_integral(f)
+
+v_hat_prime_mc = np.reshape(v_hat_prime_mc, ((2*J+1)*(2*K+1), 1))
+
+# print(np.amax(v_hat_prime - v_hat_prime_mc))
+
+
+# Compute c_ijk coefficients
+# Using Monte Carlo integration
+c_mc = np.empty([2*I+1, 2*I+1, 2*I+1], dtype = float)
+for i in range(0, 2*I+1):
+    for j in range(0, 2*I+1):
+        for k in range(0, 2*I+1):
+            f = triple_prod(phis[i], phis[j], phis[k])
+            c_mc[i, j, k] = monte_carlo_l2_integral(f)
+
+# print(np.amax(c - c_mc))
+
+
+# %%
+# Compute g_kij Riemannian metric coefficients
+# Using Monte Carlo integration
+g_mc = np.empty([2*I+1, 2*I+1, 2*I+1], dtype = float)
+for i in range(0, 2*I+1):
+            for j in range(0, 2*I+1):
+                        for k in range(0, 2*I+1):
+                                    g_mc[i,j,k] = (lamb[i] + lamb[j] - lamb[k])*c_mc[i,j,k]/2
+
+
+# Compute G_ijkl entries for the Gram operator and its dual
+# Using Monte Carlo integration
+G_mc = np.zeros([2*I+1, 2*I+1, 2*I+1, 2*I+1], dtype = float)
+G_mc = np.einsum('ikm, jlm->ijkl', c_mc, g_mc, dtype = float)
+
+G_mc = G_mc[:2*J+1, :2*K+1, :2*J+1, :2*K+1]
+G_mc = np.reshape(G_mc, ((2*J+1)*(2*K+1), (2*J+1)*(2*K+1)))
+
+G_dual_mc = np.linalg.pinv(G_mc)
+
+
+# Apply dual Gram operator G^+ to obtain v_hat 
+# Using quad integration
+v_hat_mc = np.matmul(G_dual_mc, v_hat_prime_mc)
+v_hat_mc = np.reshape(v_hat_mc, (2*J+1, 2*K+1))
+
+
+# %%
+# Apply oushforward map to v_hat to obtain approximated vector fields
+# Using Monte Carlo integration
+g_mc = g_mc[:(2*K+1), :, :]
+h_ajl_mc = np.einsum('ak, jkl -> ajl', F_k, g_mc, dtype = float)
+
+c_mc = c_mc[:(2*J+1), :, :]
+d_jlm_mc = np.einsum('ij, ilm -> jlm', v_hat_mc, c_mc, dtype = float)
+
+p_am_mc = np.einsum('ajl, jlm -> am', h_ajl_mc, d_jlm_mc, dtype = float)
+
+
+W_theta_x_mc = np.zeros(n, dtype = float)
+W_theta_y_mc = np.zeros(n, dtype = float)
+vector_approx_mc = np.empty([n, 4], dtype = float)
+
+def eigenfunc_x_mc(m):
+         return lambda x, y: p_am_mc[0,0] if m == 0 else (p_am_mc[0, m]*np.sqrt(2)*np.cos(m*np.angle(x+(1j)*y)/2) if ((m % 2) == 0 and m != 0) else p_am_mc[0, m]*np.sqrt(2)*np.sin((m+1)*np.angle(x+(1j)*y)/2))
+
+def eigenfunc_y_mc(m):
+         return lambda x, y: p_am_mc[1,0] if m == 0 else (p_am_mc[1, m]*np.sqrt(2)*np.cos(m*np.angle(x+(1j)*y)/2) if ((m % 2) == 0 and m != 0) else p_am_mc[1, m]*np.sqrt(2)*np.sin((m+1)*np.angle(x+(1j)*y)/2))
+
+def W_x_mc(args):
+            return lambda x, y: sum(eigenfunc_x_mc(a)(x, y) for a in args)
+
+def W_y_mc(args):
+            return lambda x, y: sum(eigenfunc_y_mc(a)(x, y) for a in args)
+
+for i in range(0, n):
+            W_theta_x_mc[i] = W_x_mc(list(range(0,2*I+1)))(TRAIN_X[i], TRAIN_Y[i])
+            W_theta_y_mc[i] = W_y_mc(list(range(0,2*I+1)))(TRAIN_X[i], TRAIN_Y[i])
+            vector_approx_mc[i, :] = np.array([TRAIN_X[i], TRAIN_Y[i], W_theta_x_mc[i], W_theta_y_mc[i]])
+print(W_theta_x_mc)
+print(W_theta_y_mc)
+
+X_3, Y_3, U_3, V_3 = zip(*vector_approx_mc)
+
+
+plt.figure()
+ax = plt.gca()
+# ax.quiver(X_1, Y_1, U_1, V_1, angles = 'xy', scale_units = 'xy', scale = 0.3, color = 'black')
+ax.quiver(X_3, Y_3, U_3, V_3, angles = 'xy', scale_units = 'xy', scale = 0.3, color = 'green')
+ax.set_xlim([-5,5])
+ax.set_ylim([-5,5])
+
+t = np.linspace(0, 2*np.pi, 100000)
+ax.plot(np.cos(t), np.sin(t), linewidth = 2.5, color = 'blue')
+
+plt.draw()
 plt.show()
 # %%

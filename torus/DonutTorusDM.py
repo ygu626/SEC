36e13a3e385163c406d34ede37631fa742acfd2f
Yanbing Gu <yb.gu0990@gmail.com>
2023-06-28 11:40:18 -0400

    Update DonutTorusDM.py

--- torus/DonutTorusDM.py
+++ torus/DonutTorusDM.py
@@ -29,7 +29,7 @@ N = 800         # Number of Monte Carlo training data points
 epsilon = 0.15  # RBF bandwidth parameter
 tau = 0         # Weight parameter for Laplacian eigenvalues
 alpha = 1       # Weight parameter for Markov kernel matrix
-a = 1           # Radius of the latitude circle of the torus
+a = 2           # Radius of the latitude circle of the torus
 b = 1           # Radius of the meridian circle of the torus
 
 
@@ -86,12 +86,35 @@ plt.show()
 # %%
 # n pushforward of vector field v ("arrows") on the torus
 # given points (x, y, z) specified by angle theta on the meridian circle and angle rho on the latitude circle
-THETA_LST = list(np.arange(0, 2*np.pi, np.pi/(n/2)))
-RHO_LST = list(np.arange(0, 2*np.pi, np.pi/(n/2)))
+THETA_LST = list(np.arange(0, 2*np.pi, np.pi/(50)))
+RHO_LST = list(np.arange(0, 2*np.pi, np.pi/(50)))
 
 X_func = lambda theta, rho: (a + b*np.cos(theta))*np.cos(rho)
 Y_func = lambda theta, rho: (a + b*np.cos(theta))*np.sin(rho)
 Z_func = lambda theta: b*np.sin(theta)
+
+THETA, RHO = np.meshgrid(THETA_LST, RHO_LST)
+
+x = X_func(THETA, RHO)
+y = Y_func(THETA, RHO)
+z = Z_func(THETA)
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.set_zlim(-3,3)
+ax1.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
+ax1.view_init(36, 26)
+
+ax2 = fig.add_subplot(122, projection='3d')
+ax2.set_zlim(-3,3)
+ax2.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
+ax2.view_init(0, 0)
+ax2.set_xticks([])
+
+plt.show()
+# %%
+
 
 X_func_dtheta = lambda theta, rho: -b*np.sin(theta)*np.cos(rho)
 X_func_drho = lambda theta, rho: -(a + b*np.cos(theta))*np.sin(rho)
